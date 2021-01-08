386577dab5e2344c8a8d661ee4375ac69d5403a9
mert361 <75969726+mert361@users.noreply.github.com>
2021-01-08 18:25:57 +0300

    Update and rename snn.py to NeuralNetwork.py

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -2,25 +2,28 @@
 """
 @author: Mert
 """
+#importing libraries
 import numpy as np
-
+#Using Sigmoid Normalization function to normalize data as a function.
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 
 def sigmoid_derivative(x):
     return x*(1-x)
-
+#Training input values and datas.
 training_inputs=np.array([[0,0,1],
                           [1,1,1],
                           [1,0,1],
                           [0,1,1]])
+#Training output datas as answers.
 training_outputs=np.array([[0,1,1,0]]).T
 
 np.random.seed(1)
+#Adjusting synaptic weights
 synaptic_weights=2*np.random.random((3,1))-1
 print('begining synaptic weights')
 print(synaptic_weights)
-
+#Iteration process and Error handling
 for iteration in range(100000):
     input_layer=training_inputs
     outputs=sigmoid(np.dot(input_layer,synaptic_weights))
